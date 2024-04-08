58008ef6f225ec680aa5ce29566681be4951704f
korolenko_danila <korolenko.danila1@gmail.com>
2024-04-08 10:32:49 +0300

    New interaction with items

--- Assets/Scenes/Lighthouse/Сharacters/Player/Inventory/Item/InventoryItem.cs
+++ Assets/Scenes/Lighthouse/Сharacters/Player/Inventory/Item/InventoryItem.cs
@@ -4,9 +4,16 @@ public class InventoryItem : MonoBehaviour
 {
     [SerializeField] private string _name;
     [SerializeField] private Sprite _picture;
+    private bool posibilityToRaise = false; 
 
     public Sprite GetSprite
     {
         get => _picture;
+    }
+
+    public bool PosibilityToRaise
+    {
+        get => posibilityToRaise;
+        set => posibilityToRaise = value;
     }
 }
