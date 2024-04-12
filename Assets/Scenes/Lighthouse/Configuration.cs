7775d72ed8a02fb652f30de686474680c6e82ddf
Святослав Андреев <sicome.a.s@gmail.com>
2024-04-12 13:25:19 +0300

    Drag and drop without visualizing

--- Assets/Scenes/Lighthouse/Configuration.cs
+++ Assets/Scenes/Lighthouse/Configuration.cs
@@ -8,6 +8,9 @@ namespace Assets.Scenes.Lighthouse
         public static GameObject PlayerObject { get; set; }
         public static PlayerController PlayerController { get; set; }
         public static UnityEngine.Canvas InteractionCanvas { get; set; }
+        public static UnityEngine.Canvas InventoryCanvas { get; set; }
+        public static Vector3 DragableItemTransform { get; set; }
+        public static Cell DragableCell { get; set; }
 
         private void Awake()
         {
@@ -23,6 +26,9 @@ namespace Assets.Scenes.Lighthouse
             PlayerController = PlayerObject.GetComponent<PlayerController>();
 
             InteractionCanvas = GameObject.FindGameObjectWithTag("InteractionCanvas")
+                .GetComponent<UnityEngine.Canvas>();
+
+            InventoryCanvas = GameObject.FindGameObjectWithTag("InventoryMenu")
                 .GetComponent<UnityEngine.Canvas>();
 
             DontDestroyOnLoad(gameObject);
